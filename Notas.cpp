19afd457b192663a05ad56c7d6ab936a86e21be7
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-19 01:44:38 -0500

    Notas

---
+++ Notas.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void nota1(int x);
+void nota2(int x);
+void nota3(int x);
+void nota4(int x);
+void nota5(int x);
+void nota6(int x);
+void nota7(int x);
+void nota8(int x);
+void estructura(char array[],int tam){
+    for(int i=0;i<tam;i++){
+      int x=arr[i];
+      arr[i]=arr[tam]
+    }
+};
+
+int main() {
+ char array[11]={"Do","Do#","Re","Re#/Mib","Mi","Fa","Fa#/Solb","Sol"};
+}
