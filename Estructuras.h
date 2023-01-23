f35928c47e989327980b3a10137f734f93aec698
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-23 03:08:53 -0500

    Estructuras

---
+++ Estructuras.h
@@ -0,0 +1,23 @@
+class Estructuras{
+public:
+Escala mayor{}
+EscalaMenor{}
+intervalos{}
+Conversor{}
+MenorArmonica{}
+MenorMelodica{}
+Cromatica{}
+EscalaTonosEnteros{}
+Pentatonica{}
+PentatonicaMenor{}
+
+private:
+int arr[];
+int pos;
+
+
+
+
+
+
+};
