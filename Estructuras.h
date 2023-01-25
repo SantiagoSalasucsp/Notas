0c1321fddb214d1aadda760a5b3109654c75323c
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-24 23:13:39 -0500

    Update Estructuras.h

--- Estructuras.h
+++ Estructuras.h
@@ -1,23 +1,45 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
 class Estructuras{
 public:
-Estructuras(int array,int pos)
-void EscalaMayor(){}
-void EscalaMenor(){}
-void intervalos(){}
-MenorArmonica(){}
-MenorMelodica{}
-Cromatica{}
-EscalaTonosEnteros{}
-Pentatonica{}
-PentatonicaMenor{}
+Estructuras(string arr[],int);
+int tono(string array[],int posi);
+int semitono(string array[],int posi);
+void EscalaMayor();
+void EscalaMenor();
+void intervalos();
+void Conversor();
+void MenorArmonica();
+void MenorMelodica();
+void Cromatica();
+void EscalaTonosEnteros();
+void Pentatonica();
+void PentatonicaMenor();
 
 private:
-int arr[];
+string arr[];
 int pos;
 
 
+};
 
-
-
+void EscalaMayor(string arr[],int pos){
+  int k=pos;
+  cout<<arr[k]<<endl;
+  k+=tono(arr,pos);
+  cout<<arr[k]<<endl;
+  k+=tono(arr,pos);
+  cout<<arr[k]<<endl;
+  k+=semitono(arr,pos);
+  cout<<arr[k]<<endl;
+  k+=tono(arr,pos);
+  cout<<arr[k]<<endl;
+  k+=tono(arr,pos);
+  cout<<arr[k]<<endl;
+  k+=tono(arr,pos);
+  cout<<arr[k]<<endl;
+  k+=semitono(arr,pos);
+  cout<<arr[pos]<<endl;
 
 };
